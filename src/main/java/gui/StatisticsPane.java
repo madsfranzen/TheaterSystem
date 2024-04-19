af3b85802fd8a260c5c44f0cd709746db0941de0
Mads Franzen <madsfranzen@live.dk>
2024-04-19 14:13:35 +0200

    final clean up

--- src/main/java/gui/StatisticsPane.java
+++ src/main/java/gui/StatisticsPane.java
@@ -15,8 +15,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Locale;
 
-public class StatisticsPane extends GridPane
-{
+public class StatisticsPane extends GridPane {
 
     private final ListView<Booking> lvwBookings = new ListView<>();
     private final TextArea txaSeats = new TextArea();
@@ -25,7 +24,6 @@ public class StatisticsPane extends GridPane
     private final TextField txfSuccesDate = new TextField();
     private final Alert alert = new Alert(Alert.AlertType.ERROR);
     private final TheaterGUI theaterGUI;
-
 
 
     public StatisticsPane(TheaterGUI theaterGUI) {
@@ -40,57 +38,54 @@ public class StatisticsPane extends GridPane
 
 
         Label lblShows = new Label("Customer Bookings");
-        this.add(lblShows,0,0);
+        this.add(lblShows, 0, 0);
 
         Label lblSeats = new Label("All Customer Seats and Total Price");
-        this.add(lblSeats,3,0);
+        this.add(lblSeats, 3, 0);
 
-        this.add(txaSeats,3,1,2,1);
-        txaSeats.setPrefSize(300,200);
+        this.add(txaSeats, 3, 1, 2, 1);
+        txaSeats.setPrefSize(300, 200);
         txaSeats.setEditable(false);
 
-        this.add(lvwBookings,0,1,3,1);
-        lvwBookings.setPrefSize(300,200);
-        lvwBookings.getSelectionModel().selectedIndexProperty().addListener((observable, oldValue, newValue) -> bookedSeatOnDayForShowAction()) ;
+        this.add(lvwBookings, 0, 1, 3, 1);
+        lvwBookings.setPrefSize(300, 200);
+        lvwBookings.getSelectionModel().selectedIndexProperty().addListener((observable, oldValue, newValue) -> bookedSeatOnDayForShowAction());
 
 
         Label lblBookedSeatOnDate = new Label("Number of booked seats");
-        this.add(lblBookedSeatOnDate,0,3);
-        this.add(dprDate,0,4);
-        this.add(txfBookedSeatOnDate, 0,5);
+        this.add(lblBookedSeatOnDate, 0, 3);
+        this.add(dprDate, 0, 4);
+        this.add(txfBookedSeatOnDate, 0, 5);
         txfBookedSeatOnDate.setEditable(false);
 
         Label lblSuccesDate = new Label("Succes date:");
-        this.add(lblSuccesDate,1,3);
-        this.add(txfSuccesDate,1,4);
+        this.add(lblSuccesDate, 1, 3);
+        this.add(txfSuccesDate, 1, 4);
         txfSuccesDate.setEditable(false);
 
 
         Button btnGetBookings = new Button("Get Bookings");
-        this.add(btnGetBookings,0,2);
+        this.add(btnGetBookings, 0, 2);
         btnGetBookings.setOnAction(event -> getBookingsAction());
 
         Button btnBookedSeatsOnDay = new Button("Calculate");
-        this.add(btnBookedSeatsOnDay,0,6);
+        this.add(btnBookedSeatsOnDay, 0, 6);
         btnBookedSeatsOnDay.setOnAction(event -> bookedSeatsOnDayAction());
 
         Button btnSuccesDate = new Button("Get date");
-        this.add(btnSuccesDate,1,5);
+        this.add(btnSuccesDate, 1, 5);
         btnSuccesDate.setOnAction(event -> succesDateAction());
 
     }
 
 
-    private void getBookingsAction()
-    {
+    private void getBookingsAction() {
         Show show = theaterGUI.getShowPane().getSelectedShow();
         Customer customer = theaterGUI.getCustomerPane().getSelectedCustomer();
 
-        if (customer == null)
-        {
+        if (customer == null) {
             showAlert(alert, "Please select a customer", "No customer selected");
-        } else if (customer.getBookings().isEmpty())
-        {
+        } else if (customer.getBookings().isEmpty()) {
             showAlert(alert, "Customer have no bookings", "No bookings found");
         } else {
             lvwBookings.getItems().setAll(customer.getBookings());
@@ -99,53 +94,39 @@ public class StatisticsPane extends GridPane
     }
 
 
-    private void bookedSeatsOnDayAction()
-    {
+    private void bookedSeatsOnDayAction() {
         Show show = theaterGUI.getShowPane().getSelectedShow();
         LocalDate date = dprDate.getValue();
 
-        if (show == null)
-        {
+        if (show == null) {
             showAlert(alert, "Please select a show", "No show selected");
-        } else if (date == null)
-        {
+        } else if (date == null) {
             showAlert(alert, "Please select a date", "No date selected");
-        }
-        else if (date.isBefore(show.getStartDate())) {
+        } else if (date.isBefore(show.getStartDate())) {
             showAlert(this.alert, "Booking date cannot be before shows start date.", "Invalid date");
-        }
-        else if (date.isAfter(show.getEndDate()))
-        {
+        } else if (date.isAfter(show.getEndDate())) {
             showAlert(this.alert, "Booking date cannot be after shows end date.", "Invalid date");
-        }
-        else
-        {
+        } else {
             txfBookedSeatOnDate.setText("" + show.getCountOfBookingOnDate(date));
         }
     }
 
-    private void succesDateAction()
-    {
+    private void succesDateAction() {
         Show show = theaterGUI.getShowPane().getSelectedShow();
 
-        if (show == null)
-        {
+        if (show == null) {
             showAlert(alert, "Please select a show", "No show selected");
-        } else if (show.getBookings().isEmpty())
-        {
+        } else if (show.getBookings().isEmpty()) {
             showAlert(alert, "Show doesn't have any bookings", "No bookings");
-        } else
-        {
-            txfSuccesDate.setText("" + show.getSuccesDate());
+        } else {
+            txfSuccesDate.setText("" + show.getSuccessDate());
         }
 
     }
 
 
-    private void bookedSeatOnDayForShowAction()
-    {
-        if (getSelectedBooking() != null)
-        {
+    private void bookedSeatOnDayForShowAction() {
+        if (getSelectedBooking() != null) {
             Booking booking = getSelectedBooking();
             LocalDate date = booking.getDate();
             Show show = booking.getShow();
@@ -154,8 +135,7 @@ public class StatisticsPane extends GridPane
             txaSeats.setText("");
 
             StringBuilder sb = new StringBuilder();
-            for (Seat seat : customer.getBookedSeatsForShowOnDate(show, date))
-            {
+            for (Seat seat : customer.getBookedSeatsForShowOnDate(show, date)) {
                 sb.append(String.format("%s\n", seat));
             }
 
@@ -165,13 +145,11 @@ public class StatisticsPane extends GridPane
         }
     }
 
-    private Booking getSelectedBooking()
-    {
+    private Booking getSelectedBooking() {
         return lvwBookings.getSelectionModel().getSelectedItem();
     }
 
-    private void showAlert(Alert alert, String contextText, String headerText)
-    {
+    private void showAlert(Alert alert, String contextText, String headerText) {
         alert.setHeaderText(headerText);
         alert.setContentText(contextText);
         alert.show();
